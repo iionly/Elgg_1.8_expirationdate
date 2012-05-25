242ca7e4094815e5a9806a2232bf3f6ca5984452
iionly <iionly@gmx.de>
2012-05-25 23:43:52 +0200

    Initial commit

---
+++ expirationdate/views/default/plugins/expirationdate/settings.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Expiration Date.
+ *
+ * @package ExpirationDate
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+ * @author Brett Profitt
+ * @copyright Brett Profitt 2008
+ * @link http://eschoolconsultants.com
+ */
+
+$period = elgg_get_plugin_setting('period', 'expirationdate');
+if (!$period) $period = 'fiveminute';
+// correction for typo.
+if ($period=='fifteenminute') $period = 'fifteenmin';
+?>
+<p>
+	<?php echo elgg_echo('expirationdate:period'); ?>
+
+	<?php
+		echo elgg_view('input/dropdown', array(
+			'name' => 'params[period]',
+			'options_values' => array(
+				'minute' => elgg_echo('expirationdate:minute'),
+				'fiveminute' => elgg_echo('expirationdate:fiveminute'),
+				'fifteenmin' => elgg_echo('expirationdate:fifteenminute'),
+				'halfhour' => elgg_echo('expirationdate:halfhour'),
+				'hourly' => elgg_echo('expirationdate:hourly'),
+				'daily' => elgg_echo('expirationdate:daily'),
+				'weekly' => elgg_echo('expirationdate:weekly'),
+				'monthly' => elgg_echo('expirationdate:monthly'),
+				'yearly' => elgg_echo('expirationdate:yearly'),
+			),
+			'value' => $period
+		));
+	?>
+</p>
